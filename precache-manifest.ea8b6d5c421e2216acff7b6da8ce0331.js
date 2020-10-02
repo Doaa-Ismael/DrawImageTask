9cc927f4f8a0cafccb47caf02dddc000f5480df3
Doaa Ismael <doaaismael@outlook.com>
2020-10-02 16:20:01 +0200

    Updates

---
+++ precache-manifest.ea8b6d5c421e2216acff7b6da8ce0331.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07073d097db61b5fd09f784c154e7cfd",
+    "url": "/DrawImageTask/index.html"
+  },
+  {
+    "revision": "dae87f16309484fb20b5",
+    "url": "/DrawImageTask/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "45db0f9c1584746b4a37",
+    "url": "/DrawImageTask/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dae87f16309484fb20b5",
+    "url": "/DrawImageTask/static/js/2.af333f48.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/DrawImageTask/static/js/2.af333f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45db0f9c1584746b4a37",
+    "url": "/DrawImageTask/static/js/main.d12ec915.chunk.js"
+  },
+  {
+    "revision": "25efb99074f19c5ab00c",
+    "url": "/DrawImageTask/static/js/runtime-main.97a88a80.js"
+  },
+  {
+    "revision": "e492ccf678c1f6e26be67d7fa749ca14",
+    "url": "/DrawImageTask/static/media/img1.e492ccf6.png"
+  },
+  {
+    "revision": "ed1a8749f2b5dfd1ac0b9cfdc7eb96cc",
+    "url": "/DrawImageTask/static/media/img2.ed1a8749.png"
+  },
+  {
+    "revision": "e96d69ab0675ae10b21cdce6194182f5",
+    "url": "/DrawImageTask/static/media/img3.e96d69ab.jpg"
+  }
+]);
