50046026b947b4f1eda2141e9be1bd83730ed0d0
Doaa Ismael <doaaismael@outlook.com>
2020-10-02 15:19:24 +0200

    add image and 3 buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, ButtonGroup, Col, Container, Image, Row} from "react-bootstrap";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+          <Col xs={6} md={4}>
+              <Image src={require("./assets/img1.png")} rounded data-testid="img" thumbnail />
+          </Col>
+      </Row>
+        <Row >
+            <ButtonGroup>
+                <Button variant="primary">Button 1</Button>
+                <Button variant="primary">Button 2</Button>
+                <Button variant="primary">Button 3</Button>
+            </ButtonGroup>
+        </Row>
+    </Container>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,26 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders image', () => {
+  const { getByTestId } = render(<App />);
+  const imageComponent = getByTestId("img");
+  expect(imageComponent).toBeInTheDocument();
+});
+
+
+test('renders 3 buttons', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const button1 = getByText(/Button 1/i);
+  const button2 = getByText(/Button 1/i);
+  const button3 = getByText(/Button 1/i);
+  expect(button1).toBeInTheDocument();
+  expect(button2).toBeInTheDocument();
+  expect(button3).toBeInTheDocument();
+});
+
+test('renders first image as default', () => {
+  const { getByTestId } = render(<App />);
+  const image = getByTestId("img");
+  console.log("image ", image.getAttribute("src"));
+  expect(image.getAttribute("src")).toMatch(/img1.png/);
 });
